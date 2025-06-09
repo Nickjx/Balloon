a9a8e039c8caee485262823aa1482b8b7d75325a
Roma Boiko <102807041+Nickjx@users.noreply.github.com>
2025-06-09 16:32:41 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,66 @@
+const balloonEl = document.querySelector('.ballon');
+const styleSheet = new CSSStyleSheet();
+
+// console.dir(boxEl);
+// console.dir(balloonEl);
+
+//* Make our page preetier
+
+styleSheet.replaceSync(`
+  * {
+  box-sizing: border-box;
+  font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+}
+body {
+  margin: 0;
+  overflow-y: hidden;
+  overflow-x: hidden;
+}
+.flx{
+  flex-direction: column;
+}
+.head{
+    display: flex;
+    justify-content: center;
+    color: rgb(255, 162, 1);
+}
+.head span{
+    color: rgb(151, 199, 7);
+}
+
+.box-element{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+  .ballon {
+    font-size: 30px;
+
+}`);
+
+document.adoptedStyleSheets = [styleSheet];
+
+//* main logic of program
+
+addEventListener('keyup', changeSize);
+
+function changeSize(e) {
+  const currentSizeEl = parseFloat(getComputedStyle(balloonEl).fontSize);
+  if (e.key === 'ArrowUp') {
+    //* зміна розміру кульки
+    if (currentSizeEl <= 99) {
+      balloonEl.style.fontSize = `${currentSizeEl + 10}px`;
+    } else {
+      boom();
+    }
+  } else if (e.key === 'ArrowDown') {
+    if (currentSizeEl > 30) {
+      balloonEl.style.fontSize = `${currentSizeEl - 10}px`;
+    }
+  }
+}
+
+function boom() {
+  balloonEl.textContent = '💥';
+  removeEventListener('keyup', changeSize);
+}
